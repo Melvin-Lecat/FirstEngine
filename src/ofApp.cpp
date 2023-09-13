260579a5bdc1662b7450b9da65704715eeb7850b
Newg <g_robert@insa-toulouse.fr>
2023-09-13 15:45:28 -0400

    Ajout des vecteurs

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -1,71 +1,93 @@
 #include "ofApp.h"
+#include <stdlib.h>
+#define RAD 30
+//--------------------------------------------------------------
+void ofApp::setup() {
+	ofSetColor(255, 255, 255);
+}
 
 //--------------------------------------------------------------
-void ofApp::setup(){
+void ofApp::update() {
+}
+
+//--------------------------------------------------------------
+void ofApp::draw() {
+
+	ofDrawCircle(this->pos.v2(), RAD);
+}
+
+//--------------------------------------------------------------
+void ofApp::keyPressed(int key) {
+	switch (key)
+	{
+	case 'z':
+		ofApp::UpdateParticle(Vector::OneY().Invert());
+		break;
+	case 's':
+		ofApp::UpdateParticle(Vector::OneY());
+		break;
+	case 'q':
+		ofApp::UpdateParticle(Vector::OneX().Invert());
+		break;
+	case 'd':
+		ofApp::UpdateParticle(Vector::OneX());
+		break;
+	default:
+		break;
+	}
+}
+
+//--------------------------------------------------------------
+void ofApp::keyReleased(int key) {
 
 }
 
 //--------------------------------------------------------------
-void ofApp::update(){
+void ofApp::mouseMoved(int x, int y) {
 
 }
 
 //--------------------------------------------------------------
-void ofApp::draw(){
+void ofApp::mouseDragged(int x, int y, int button) {
 
 }
 
 //--------------------------------------------------------------
-void ofApp::keyPressed(int key){
+void ofApp::mousePressed(int x, int y, int button) {
 
 }
 
 //--------------------------------------------------------------
-void ofApp::keyReleased(int key){
+void ofApp::mouseReleased(int x, int y, int button) {
 
 }
 
 //--------------------------------------------------------------
-void ofApp::mouseMoved(int x, int y ){
+void ofApp::mouseEntered(int x, int y) {
 
 }
 
 //--------------------------------------------------------------
-void ofApp::mouseDragged(int x, int y, int button){
+void ofApp::mouseExited(int x, int y) {
 
 }
 
 //--------------------------------------------------------------
-void ofApp::mousePressed(int x, int y, int button){
+void ofApp::windowResized(int w, int h) {
 
 }
 
 //--------------------------------------------------------------
-void ofApp::mouseReleased(int x, int y, int button){
+void ofApp::gotMessage(ofMessage msg) {
 
 }
 
 //--------------------------------------------------------------
-void ofApp::mouseEntered(int x, int y){
+void ofApp::dragEvent(ofDragInfo dragInfo) {
 
 }
 
-//--------------------------------------------------------------
-void ofApp::mouseExited(int x, int y){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::windowResized(int w, int h){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::gotMessage(ofMessage msg){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::dragEvent(ofDragInfo dragInfo){ 
-
+void ofApp::UpdateParticle(Vector dir) {
+	std::cout << "Update" << std::endl;
+	this->pos = this->pos + dir * 10;
 }
