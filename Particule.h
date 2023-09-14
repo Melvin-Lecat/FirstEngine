d72cfd3ee9b42047b86b21e15d0a78247b107dde
p2018476 <melvin.lecat@etu.univ-lyon1.fr>
2023-09-14 16:30:58 -0400

    Petites corrections

--- Particule.h
+++ Particule.h
@@ -2,42 +2,43 @@
 #include "ofMain.h"
 #include "Vector.h"
 
-class Particule {
-
+class Particule
+{
 private:
-	Vector position;
-	Vector velocite;
-	Vector acceleration;
-	float masse;
+    Vector position;
+    Vector velocite;
+    Vector acceleration;
+    float masse;
 
 public:
-	Particule() {
-		//
-	}
+    Particule()
+    {
+        //
+    }
 
-	Particule(Vector position, Vector velocite, Vector acceleration, float masse) {
-		this->position = position;
-		this->velocite = velocite;
-		this->acceleration = acceleration;
-		this->masse = 1/masse;
-	}
+    Particule(Vector position, Vector velocite, Vector acceleration, float masse)
+    {
+        this->position = position;
+        this->velocite = velocite;
+        this->acceleration = acceleration;
+        this->masse = 1 / masse;
+    }
 
-	void push(Particule p1) { //sert d'update
-		//vitesse initiale
-		float vitinit;
+    void push(Particule p1)
+    {
+        //sert d'update
+        //vitesse initiale
+        float vitinit;
 
-		//angle de la trajectoire
-		float angle;
+        //angle de la trajectoire
+        float angle;
 
-		velocite.v3().x = vitinit * cos(angle);
-		velocite.v3().y = vitinit * sin(angle);
-		velocite.v3().z = 0;
+        velocite.v3().x = vitinit * cos(angle);
+        velocite.v3().y = vitinit * sin(angle);
+        velocite.v3().z = 0;
 
-		position.v3().x = vitinit * cos(angle); // * t (en fonction des frames)
-		position.v3().x = 1/2*t*g	vitinit * sin(angle); // * t (en fonction des frames)
-		position.v3().z = 0; // * t (en fonction des frames)
-
-
-
-	}
-}
+        position.v3().x = vitinit * cos(angle); // * t (en fonction des frames)
+        position.v3().x = 1 / 2 * t * g + vitinit * sin(angle); // * t (en fonction des frames)
+        position.v3().z = 0; // * t (en fonction des frames)
+    }
+};
