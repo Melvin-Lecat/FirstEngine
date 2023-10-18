d900259eb83d8e54bbd3c4f179132d21ef77c68e
aurelien.dufay <dufaya@3il.fr>
2023-10-18 15:42:46 -0400

    Classe Spring

---
+++ src/ParticleSpringHook.h
@@ -0,0 +1,17 @@
+#pragma once
+#include "Particle.h"
+#include "ParticleForceGenerator.h"
+#include "Vector.h"
+
+class ParticleSpringHook:public ParticleForceGenerator
+{
+public:
+    float K;
+    float length;
+    Particle *particle;
+    Vector point;
+    //ParticleSpringHook(Particle *particle1, Particle *particle2, float K, float length);
+    ParticleSpringHook(Particle *particle, Vector point, float K, float length);
+    float distance();
+    void updateForce(Particle *particle, float duration);
+};
