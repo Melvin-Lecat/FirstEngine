ff80f9730b1a824bc4e42dbc7ceaab7979196417
Newg <g_robert@insa-toulouse.fr>
2023-10-23 10:12:52 -0400

    ajout de l'elastique

---
+++ src/ParticleElastic.cpp
@@ -0,0 +1,13 @@
+﻿#include "ParticleElastic.h"
+
+void ParticleElastic::updateForce(Particle* particle, float duration)
+{
+    cout << "Current length: " << particle->position.distance(point) << endl;
+    cout << "Spring Force: " << glm::abs(length - particle->position.distance(point)) << endl;
+    if (particle->position.distance(point) > length)
+    {
+        // Direction * K * longueur du ressort
+            particle->addForce(
+                (this->point - particle->position).normalized() * K * glm::abs(length - particle->position.distance(point)));
+    }
+}
---
+++ src/ParticleElastic.h
@@ -0,0 +1,9 @@
+﻿#pragma once
+#include "FixedSpringGenerator.h"
+
+class ParticleElastic: public FixedSpringGenerator
+{
+public:
+    void updateForce(Particle* particle, float duration) override;
+    
+};
